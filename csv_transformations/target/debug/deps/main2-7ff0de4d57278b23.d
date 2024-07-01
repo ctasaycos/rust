e0186cf861f86a9a646dd3de1428f9519c956b31
ctasaycos <ctasaycosilva@gmail.com>
2024-06-30 19:35:26 -0500

    s

---
+++ csv_transformations/target/debug/deps/main2-7ff0de4d57278b23.d
@@ -0,0 +1,5 @@
+/Users/carlostasaycosilva/Library/Mobile Documents/com~apple~CloudDocs/rust/project1/csv_transformations/target/debug/deps/libmain2-7ff0de4d57278b23.rmeta: src/bin/main2.rs
+
+/Users/carlostasaycosilva/Library/Mobile Documents/com~apple~CloudDocs/rust/project1/csv_transformations/target/debug/deps/main2-7ff0de4d57278b23.d: src/bin/main2.rs
+
+src/bin/main2.rs:
--- csv_transformations/target/debug/deps/my_rust_project-0f21e904be8199e0.d
+++ csv_transformations/target/debug/deps/my_rust_project-0f21e904be8199e0.d
@@ -1,6 +1,5 @@
-/Users/carlostasaycosilva/Library/Mobile Documents/com~apple~CloudDocs/rust/project1/csv_transformations/target/debug/deps/libmy_rust_project-0f21e904be8199e0.rmeta: src/main.rs src/main2.rs
+/Users/carlostasaycosilva/Library/Mobile Documents/com~apple~CloudDocs/rust/project1/csv_transformations/target/debug/deps/libmy_rust_project-0f21e904be8199e0.rmeta: src/main.rs
 
-/Users/carlostasaycosilva/Library/Mobile Documents/com~apple~CloudDocs/rust/project1/csv_transformations/target/debug/deps/my_rust_project-0f21e904be8199e0.d: src/main.rs src/main2.rs
+/Users/carlostasaycosilva/Library/Mobile Documents/com~apple~CloudDocs/rust/project1/csv_transformations/target/debug/deps/my_rust_project-0f21e904be8199e0.d: src/main.rs
 
 src/main.rs:
-src/main2.rs:
